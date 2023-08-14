06ff6efb6e7f6808f0d9c538db0f465f484ae507
ModupeD <daniemu@mail.uc.edu>
2023-08-13 23:36:25 -0400

    added types

---
+++ components/Tweet.tsx
@@ -0,0 +1,47 @@
+import {View, Text, Image, StyleSheet} from 'react-native';
+import { TweetType } from '../types';
+
+type TweetProps = {
+  tweet: TweetType;
+}
+const Tweet = ({ tweet }: TweetProps) => {
+  console.log(tweet.user.image?.toLowerCase())
+    return (
+        <View style={styles.container}>
+        <Image src={tweet.user.image} style={styles.userImage} />
+
+        <View style={styles.mainContainer}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.content}>{tweet.content}</Text>
+        </View>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection:'row',
+      padding: 10,
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      borderColor:'lightgrey',
+      backgroundColor:'white'
+    },
+    userImage: {
+      width:50,
+      height:50,
+      borderRadius: 50
+    },
+    mainContainer:{
+      flex:1,
+      marginLeft:10,
+    },
+    name: {
+      fontWeight:'600'
+    },
+    content: {
+      lineHeight: 20,
+      marginTop: 5,
+    }
+  });
+
+  export default Tweet;
